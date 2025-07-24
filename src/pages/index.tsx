9f6d00ac2ee8f4257164f38ecf3d6f0b94c1a486
zanit-solution <zanltd.bd@gmail.com>
2025-07-24 19:40:21 +0600

    fix:smoke cursor added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,15 @@
 import About from "@/components/about/About";
 import Contact from "@/components/contact/Contact";
 import Exprience from "@/components/exprience/Exprience";
-import ProjectCard from "@/components/ProjectCard/ProjectCard"; 
+import ProjectCard from "@/components/ProjectCard/ProjectCard";
+import Cursor from "@/components/ui/Cursor";
 import PageLayout from "@/components/ui/pageLayout";
 import React, { useEffect, useState } from "react";
 const Home = () => {
   return (
     <>
+    <Cursor/>
+
       <PageLayout>
         <About />
       </PageLayout>
