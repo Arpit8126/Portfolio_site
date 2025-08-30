10016c4bcf8993f4bc3541855674fba66d1c7d8b
abdulmalek <abdulmalek.swe.585@gmail.com>
2025-08-30 19:00:40 +0600

    fix:solve issue

--- src/constants/project.ts
+++ src/constants/project.ts
@@ -5,7 +5,7 @@ export const projectList: projectType[] = [
     logo: "/project/1.png",
     title: "Bajar Net (Multivendor Ecomerce)",
     description:
-      "A Multivendor ecomerce websiteA Multivendor ecomerce websiteA Multivendor ecomerce websiteA Multivendor ecomerce websiteA Multivendor ecomerce websiteA Multivendor ecomerce websiteA Multivendor ecomerce websiteA Multivendor ecomerce websiteA Multivendor ecomerce websiteA Multivendor ecomerce website",
+      "A Multivendor ecomerce websiteA Multivendor quiecomerce ecomerce websiteA Multivendor ecomerce websiteA Multivendor ecomerce websiteA Multivendor ecomerce websiteA Multivendor ecomerce websiteA Multivendor ecomerce websiteA Multivendor ecomerce websiteA Multivendor ecomerce websiteA Multivendor ecomerce website",
     live_link: "https://multivendor-dun.vercel.app/",
     github_link: "https://github.com/AbdulMalek-swe/multivendor",
     stack_image: [
